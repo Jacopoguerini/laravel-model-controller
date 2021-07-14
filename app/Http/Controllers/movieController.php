bc85e2a35a3fa583e96b414b4f601471b25116b8
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-07-14 16:00:24 +0200

    Corretto nome MovieController (PascalCase)

--- app/Http/Controllers/movieController.php
+++ app/Http/Controllers/movieController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Movie;
 
-class movieController extends Controller
+class MovieController extends Controller
 {
     public function index() {
         $title = "Lista Film";
